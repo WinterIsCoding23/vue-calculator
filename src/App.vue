274d91c4c05b2bb92373b61961be834bf252ab56
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-17 12:35:42 +0200

    add buttons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <script></script>
 
-<template></template>
+<template>
+  <h1>1234567890</h1>
+  <button type="button" value="">C</button>
+  <button type="button" value="">+/-</button>
+  <button type="button" value="">%</button>
+  <button type="button" value="">/</button>
+  <button type="button" value="">7</button>
+  <button type="button" value="">8</button>
+  <button type="button" value="">9</button>
+  <button type="button" value="">*</button>
+  <button type="button" value="">4</button>
+  <button type="button" value="">5</button>
+  <button type="button" value="">6</button>
+  <button type="button" value="">-</button>
+  <button type="button" value="">1</button>
+  <button type="button" value="">2</button>
+  <button type="button" value="">3</button>
+  <button type="button" value="">+</button>
+  <button type="button" value="">0</button>
+</template>
 
 <style></style>
