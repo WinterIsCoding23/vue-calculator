fe673e220d39d0ef037caa107d71d27eec28c919
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-17 18:55:32 +0200

    rename class buttons into button-field & replace class button with tag button

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,29 @@
     <div class="display">
       <h1 class="text-8xl font-light overflow-x-auto">1234567890</h1>  
     </div>
-    <div class="buttons">
+    <div class="button-field">
       
         <button type="button" class="button" value="">C</button>
         <button type="button" class="button" value="">%</button>
         <button type="button" class="button" value="/">÷</button>
         <button type="button" class="button" value="*">×</button>     
       
-        <button type="button" class="button" value="">7</button>
-        <button type="button" class="button" value="">8</button>
-        <button type="button" class="button" value="">9</button>
+        <button type="button" value="">7</button>
+        <button type="button" value="">8</button>
+        <button type="button" value="">9</button>
         <button type="button" class="button" value="">+</button>      
       
-        <button type="button" class="button" value="">4</button>
-        <button type="button" class="button" value="">5</button>
-        <button type="button" class="button" value="">6</button>
+        <button type="button" value="">4</button>
+        <button type="button" value="">5</button>
+        <button type="button" value="">6</button>
         <button type="button" class="button" value="">-</button>      
       
-        <button type="button" class="button" value="">1</button>
-        <button type="button" class="button" value="">2</button>
-        <button type="button" class="button" value="">3</button>
+        <button type="button" value="">1</button>
+        <button type="button" value="">2</button>
+        <button type="button" value="">3</button>
         <button type="button" class="min-h-[50px] row-span-2 border-2 border-orange-600 rounded-md bg-red-600" value="">=</button>      
       
-        <button type="button" class="button" value="">0</button>
+        <button type="button" value="">0</button>
         <button type="button" class="button" value="">+/-</button>
         <button type="button" class="button" value="">.</button>
       
