c476934ce10449be1ebe306f6b2b5d8e9cedb091
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-17 16:54:25 +0200

    replace / and *

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,18 @@
       
         <button type="button" value="">C</button>
         <button type="button" value="">%</button>
-        <button type="button" value="">/</button>
-        <button type="button" value="">*</button>     
+        <button type="button" value="/">÷</button>
+        <button type="button" value="*">×</button>     
       
         <button type="button" value="">7</button>
         <button type="button" value="">8</button>
         <button type="button" value="">9</button>
-        <button type="button" value="">-</button>      
+        <button type="button" value="">+</button>      
       
         <button type="button" value="">4</button>
         <button type="button" value="">5</button>
         <button type="button" value="">6</button>
-        <button type="button" value="">+</button>      
+        <button type="button" value="">-</button>      
       
         <button type="button" value="">1</button>
         <button type="button" value="">2</button>
