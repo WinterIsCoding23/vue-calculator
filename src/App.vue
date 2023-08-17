f55b55a54349db5ce0fc68eadc90594c16c4a8d5
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-17 16:48:04 +0200

    organize elements in flexbox col centered

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,22 @@
         <button type="button" value="">C</button>
         <button type="button" value="">%</button>
         <button type="button" value="">/</button>
-        <button type="button" value="">*</button>
-     
+        <button type="button" value="">*</button>     
       
         <button type="button" value="">7</button>
         <button type="button" value="">8</button>
         <button type="button" value="">9</button>
-        <button type="button" value="">-</button>
-      
+        <button type="button" value="">-</button>      
       
         <button type="button" value="">4</button>
         <button type="button" value="">5</button>
         <button type="button" value="">6</button>
-        <button type="button" value="">+</button>
-      
+        <button type="button" value="">+</button>      
       
         <button type="button" value="">1</button>
         <button type="button" value="">2</button>
         <button type="button" value="">3</button>
-        <button type="button" class="row-span-2" value="">=</button>
-      
+        <button type="button" class="row-span-2" value="">=</button>      
       
         <button type="button" value="">0</button>
         <button type="button" value="">+/-</button>
