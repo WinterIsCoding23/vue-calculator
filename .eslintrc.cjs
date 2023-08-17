f5577e356b4f0ae2631c25a93494b773c0da755e
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-17 16:20:59 +0200

    fix tailwind & try to fix prettier & create buttons

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,19 +4,22 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:vue/vue3-recommended',
+    // 'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended'
+    // 'prettier'
   ],
   overrides: [
     {
-      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      files: ['cypress/e2e/**/*.{cy,spec}.{vue,css,js,ts,jsx,tsx}'],
       extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
+  plugins: ['vue', 'html', 'prettier'],
   env: {
     node: true
   }
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <script></script>
 
 <template>
-  <h1>1234567890</h1>
-  <button type="button" value="">C</button>
-  <button type="button" value="">+/-</button>
-  <button type="button" value="">%</button>
-  <button type="button" value="">/</button>
-  <button type="button" value="">7</button>
-  <button type="button" value="">8</button>
-  <button type="button" value="">9</button>
-  <button type="button" value="">*</button>
-  <button type="button" value="">4</button>
-  <button type="button" value="">5</button>
-  <button type="button" value="">6</button>
-  <button type="button" value="">-</button>
-  <button type="button" value="">1</button>
-  <button type="button" value="">2</button>
-  <button type="button" value="">3</button>
-  <button type="button" value="">+</button>
-  <button type="button" value="">0</button>
-  <button type="button" value="">.</button>
-  <button type="button" value="">=</button>
+  <div class="calculator">
+    <h1 class="border-solid border-2 border-indigo-500/50 rounded-md">1234567890</h1>  
+    <div class="buttons">
+      <div class="row">
+        <button type="button" value="">C</button>
+        <button type="button" value="">%</button>
+        <button type="button" value="">/</button>
+        <button type="button" value="">*</button>
+      </div>
+      <div class="row">
+        <button type="button" value="">7</button>
+        <button type="button" value="">8</button>
+        <button type="button" value="">9</button>
+        <button type="button" value="">-</button>
+      </div>
+      <div class="row">
+        <button type="button" value="">4</button>
+        <button type="button" value="">5</button>
+        <button type="button" value="">6</button>
+        <button type="button" value="">+</button>
+      </div>
+      <div class="row">
+        <button type="button" value="">1</button>
+        <button type="button" value="">2</button>
+        <button type="button" value="">3</button>
+        <button type="button" value="">=</button>
+      </div>
+      <div class="row">
+        <button type="button" value="">0</button>
+        <button type="button" value="">+/-</button>
+        <button type="button" value="">.</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
