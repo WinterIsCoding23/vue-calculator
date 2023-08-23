da212af0259dc8a6657495d6299b8ce74fd32dd7
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-23 17:59:36 +0200

    delete comments and unused variables & add switch-statement to calculate but still not working

--- src/App.vue
+++ src/App.vue
@@ -35,90 +35,84 @@
 
 <script>
   export default {   
-    // mounted(){
-    //   const displayContent = this.$refs.display; 
-    // },
       data() {
         return{
           previous: null,
-          current: "",
-          numberClicked: false,
-          isNegative: false,
-          hasBeenCalculated: false,
-          result: null,
-          // new try:
           calculatorDisplay: "0",
           displayValue: 0,
           isAwaitingInput: false,
-          operate: {
-            "+": (first, second) => first + second,
-            "-": (first, second) => first - second,
-            "/": (first, second) => first / second,
-            "*": (first, second) => first * second,
-            "=": (first, second) => first,
-          },
           operatorValue: "",
         }
       },
-      computed: {
-        // const displayContent = this.$refs.display.textContent; 
-      },  
       methods: {
-        displayNumber(number){
-          if(this.isAwaitingInput){
-            this.calculatorDisplay = number;
-            this.isAwaitingInput = false;
+        displayNumber(number) {
+        if (this.isAwaitingInput) {
+          this.calculatorDisplay = number;
+          this.isAwaitingInput = false;
+        } else {
+          if (this.operatorValue) {
+            this.calculatorDisplay += number; 
           } else {
             this.displayValue = this.calculatorDisplay;
-            this.calculatorDisplay =
-              this.displayValue === "0" ? number : this.displayValue + number;
+            this.calculatorDisplay = this.displayValue === "0" ? number : this.displayValue + number;
           }
-          this.displayValue = this.calculatorDisplay;
+        }
+        this.displayValue = this.calculatorDisplay;
         },
+
         toggleNegative(){          
           this.displayValue = (parseFloat(this.displayValue) * -1).toString();
           this.calculatorDisplay = this.displayValue;
         },     
         period(){
           if (this.displayValue.includes(".")){
-            return 
+            return; 
           } else {
             this.displayValue = `${this.displayValue}.`;
             this.calculatorDisplay = this.displayValue;
           }
         },
-        calculate(operator){
-          const followingValue = parseFloat(this.calculatorDisplay);
-          // console.log("operator", operator);
-          console.log("FirstfollowingValue", followingValue);
-          // console.log("displayValue", this.displayValue);
-          if(this.operatorValue && this.isAwaitingInput){
+        setPrevious(){
+          this.previous = this.displayValue;
+          console.log("previous", this.previous);
+          console.log("previous-displayValue", this.displayValue);
+          this.isAwaitingInput = true;
+          return this.previous;
+        },
+        calculate(operator) {
+          if (this.operatorValue && this.isAwaitingInput) {
             this.operatorValue = operator;
-            // console.log("operatorValue", this.operatorValue);            
             return;
           }
-          
-          if(!this.displayValue){
-            this.displayValue = followingValue;
+
+          if (!this.operatorValue) {
+            this.setPrevious(); 
           } else {
-            this.displayValue = parseFloat(this.displayValue);
-            this.operatorValue = operator;
-            const calculation = this.operate[ this.operatorValue](this.displayValue, followingValue);
-            console.log("calculation", calculation);
-            // console.log("operator", operator);
-            // console.log("operatorValue", this.operatorValue);            
-            console.log("displayValue", this.displayValue);
-            console.log("followingValue", followingValue);
-            this.calculatorDisplay = calculation;
-            this.displayValue = calculation;
+            const followingValue = parseFloat(this.calculatorDisplay);
+            
+            switch (this.operatorValue) {
+              case "+":
+                this.displayValue = this.previous + followingValue;
+                console.log("displayValue in switch", this.displayValue);
+                this.setPrevious(); 
+                break;
+              case "-":
+                this.displayValue = this.previous - followingValue;
+                break;
+              case "*":
+                this.displayValue = this.previous * followingValue;
+                break;
+              case "/":
+                this.displayValue = this.previous / followingValue;
+                break;             
+            }
           }
-          this.isAwaitingInput = true;
           this.operatorValue = operator;
-          console.log("operatorValue", this.operatorValue);            
-          console.log("type of operatorValue", typeof(this.operatorValue));            
-        },   
-      }    
-  }
+          this.calculatorDisplay += operator;
+          this.isAwaitingInput = true;
+        }
+      }
+  }    
 </script>
 
 <style></style>
