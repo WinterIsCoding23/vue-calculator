82004ebef1afe80970da6786bc4c2f345ca0c6b7
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-23 14:22:52 +0200

    rename operator-object in operate & rename operate-function in calculate

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,23 @@
       
         <button @click="reset" type="button" class="operator" value="">C</button>
         <button @click="percentage" type="button" class="operator" value="">%</button>
-        <button @click="operate('/')" type="button" class="operator" value="/">÷</button>
-        <button @click="operate('*')" type="button" class="operator" value="*">×</button>     
+        <button @click="calculate('/')" type="button" class="operator" value="/">÷</button>
+        <button @click="calculate('*')" type="button" class="operator" value="*">×</button>     
       
         <button @click="displayNumber('7')" type="button" value="7">7</button>
         <button @click="displayNumber('8')" type="button" value="">8</button>
         <button @click="displayNumber('9')" type="button" value="">9</button>
-        <button @click="operate('+')" type="button" class="operator" value="+">+</button>      
+        <button @click="calculate('+')" type="button" class="operator" value="+">+</button>      
       
         <button @click="displayNumber('4')" type="button" value="">4</button>
         <button @click="displayNumber('5')" type="button" value="">5</button>
         <button @click="displayNumber('6')" type="button" value="">6</button>
-        <button @click="operate('-')" type="button" class="operator" value="">-</button>      
+        <button @click="calculate('-')" type="button" class="operator" value="">-</button>      
       
         <button @click="displayNumber('1')" type="button" value="">1</button>
         <button @click="displayNumber('2')" type="button" value="">2</button>
         <button @click="displayNumber('3')" type="button" value="">3</button>
-        <button @click="calculate" type="button" class="min-h-[50px] row-span-2 border-2 border-orange-600 rounded-md bg-red-600 text-white" value="">=</button>      
+        <button @click="process" type="button" class="min-h-[50px] row-span-2 border-2 border-orange-600 rounded-md bg-red-600 text-white" value="">=</button>      
       
         <button @click="displayNumber('0')" type="button" value="">0</button>
         <button @click="toggleNegative" type="button" class="button" value="">+/-</button>
@@ -50,7 +50,7 @@
           calculatorDisplay: "0",
           displayValue: "0",
           isAwaitingInput: false,
-          operator: {
+          operate: {
             "+": (first, second) => first + second,
             "-": (first, second) => first - second,
             "/": (first, second) => first / second,
@@ -67,16 +67,11 @@
             this.calculatorDisplay = number;
             this.isAwaitingInput = false;
             this.displayValue = this.calculatorDisplay;
-            console.log("calculatorDisplay", this.calculatorDisplay);
-            console.log("number", number);
           } else {
             this.displayValue = this.calculatorDisplay;
             this.calculatorDisplay =
               this.displayValue === "0" ? number : this.displayValue + number;
             this.displayValue = this.calculatorDisplay;
-            console.log("displayValue", this.displayValue);
-            console.log("calculatorDisplay", this.calculatorDisplay);
-            console.log("number", number);
           }
         },
         toggleNegative(){          
@@ -91,7 +86,7 @@
             this.calculatorDisplay = this.displayValue;
           }
         },
-        operate(operator){
+        calculate(operator){
           this.calculatorDisplay
           this.displayValue
         },   
