607efd4bb48cae19d74a04e17bea5ac09857a593
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-23 12:45:15 +0200

    starting to redesign functionality - creating calculatorDisplay and isAwaitingInput & applying them to new method displayNumber

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="calculator">
     <div class="display ">
-      <h1 class="h-min text-7xl font-light overflow-x-auto rounded-md p-4">{{ current || 0 }}</h1>  
+      <h1 v-text="calculatorDisplay" class="h-min text-7xl font-light overflow-x-auto rounded-md p-4"></h1>  
     </div>
     <div class="button-field">
       
@@ -10,22 +10,22 @@
         <button @click="operate('/')" type="button" class="operator" value="/">÷</button>
         <button @click="operate('*')" type="button" class="operator" value="*">×</button>     
       
-        <button @click="display(7)" type="button" value="7">7</button>
-        <button @click="display(8)" type="button" value="">8</button>
-        <button @click="display(9)" type="button" value="">9</button>
+        <button @click="displayNumber(7)" type="button" value="7">7</button>
+        <button @click="displayNumber(8)" type="button" value="">8</button>
+        <button @click="displayNumber(9)" type="button" value="">9</button>
         <button @click="operate('+')" type="button" class="operator" value="+">+</button>      
       
-        <button @click="display(4)" type="button" value="">4</button>
-        <button @click="display(5)" type="button" value="">5</button>
-        <button @click="display(6)" type="button" value="">6</button>
+        <button @click="displayNumber(4)" type="button" value="">4</button>
+        <button @click="displayNumber(5)" type="button" value="">5</button>
+        <button @click="displayNumber(6)" type="button" value="">6</button>
         <button @click="operate('-')" type="button" class="operator" value="">-</button>      
       
-        <button @click="display(1)" type="button" value="">1</button>
-        <button @click="display(2)" type="button" value="">2</button>
-        <button @click="display(3)" type="button" value="">3</button>
+        <button @click="displayNumber(1)" type="button" value="">1</button>
+        <button @click="displayNumber(2)" type="button" value="">2</button>
+        <button @click="displayNumber(3)" type="button" value="">3</button>
         <button @click="calculate" type="button" class="min-h-[50px] row-span-2 border-2 border-orange-600 rounded-md bg-red-600 text-white" value="">=</button>      
       
-        <button @click="display(0)" type="button" value="">0</button>
+        <button @click="displayNumber(0)" type="button" value="">0</button>
         <button @click="negative" type="button" class="button" value="">+/-</button>
         <button @click="period" type="button" class="button" value="">.</button>
       
@@ -35,6 +35,9 @@
 
 <script>
   export default {   
+    // mounted(){
+    //   const displayContent = this.$refs.display; 
+    // },
       data() {
         return{
           previous: null,
@@ -43,26 +46,28 @@
           isNegative: false,
           hasBeenCalculated: false,
           result: null,
+          // new try:
+          calculatorDisplay: 0,
+          isAwaitingInput: true,
         }
       },
+      computed: {
+        // const displayContent = this.$refs.display.textContent; 
+      },  
       methods: {
-        display(number){
-          if(this.hasBeenCalculated){
-            this.reset();
-            this.current = number;
-            this.hasBeenCalculated = false;
-            console.log("hasBeenCalculated", this.hasBeenCalculated);
-          } else{
-            if(this.previous){
-              this.current = this.previous.toString().concat("", number);
-            } else {
-              this.current = number;
-            }
+        displayNumber(number){
+          if(this.isAwaitingInput){
+            this.calculatorDisplay = number;
+            this.isAwaitingInput = false;
+            console.log("calculatorDisplay", this.calculatorDisplay);
+            console.log("number", number);
+          } else {
+            // const displayValue = this.displayContent;
+            // this.displayContent =
+            //   displayValue === "0" ? number : displayValue + number;
+            // console.log("displayValue", displayValue);
+            // console.log("number", number);
           }
-          this.numberClicked = true;  
-          this.previous = this.current;
-          console.log("this.current", this.current);
-          console.log("this.previous", this.previous);
         },
         negative(){
           if(this.isNegative){
