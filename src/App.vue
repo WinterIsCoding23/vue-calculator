42df0fa6ad0f3ce7b4ea2be22adae2e4748550eb
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-25 13:29:10 +0200

    rename add to operate and apply it to other operators & rename calculate to displayResult

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,23 @@
       
         <button @click="reset" type="button" class="operator" value="">C</button>
         <button @click="percentage" type="button" class="operator" value="">%</button>
-        <button @click="" type="button" class="operator" value="/">÷</button>
-        <button @click="" type="button" class="operator" value="*">×</button>     
+        <button @click="operate('/')" type="button" class="operator" value="/">÷</button>
+        <button @click="operate('*')" type="button" class="operator" value="*">×</button>     
       
         <button @click="displayNumber('7')" type="button" value="7">7</button>
         <button @click="displayNumber('8')" type="button" value="">8</button>
         <button @click="displayNumber('9')" type="button" value="">9</button>
-        <button @click="add('+')" type="button" class="operator" value="+">+</button>      
+        <button @click="operate('+')" type="button" class="operator" value="+">+</button>      
       
         <button @click="displayNumber('4')" type="button" value="">4</button>
         <button @click="displayNumber('5')" type="button" value="">5</button>
         <button @click="displayNumber('6')" type="button" value="">6</button>
-        <button @click="add('-')" type="button" class="operator" value="-">-</button>      
+        <button @click="operate('-')" type="button" class="operator" value="-">-</button>      
       
         <button @click="displayNumber('1')" type="button" value="">1</button>
         <button @click="displayNumber('2')" type="button" value="">2</button>
         <button @click="displayNumber('3')" type="button" value="">3</button>
-        <button @click="calculate" type="button" class="min-h-[50px] row-span-2 border-2 border-orange-600 rounded-md bg-red-600 text-white" value="">=</button>      
+        <button @click="displayResult" type="button" class="min-h-[50px] row-span-2 border-2 border-orange-600 rounded-md bg-red-600 text-white" value="">=</button>      
       
         <button @click="displayNumber('0')" type="button" value="">0</button>
         <button @click="toggleNegative" type="button" class="button" value="">+/-</button>
@@ -43,6 +43,7 @@
           operatorWasClicked: false,
           operatorValue: null,
           operatorClicked: null,
+          isAwaitingNumber: true,
         }
       },
       methods: {
@@ -61,6 +62,7 @@
                 break;
               }
             }
+
             if (this.operatorClicked === "+") {
               this.calculatorDisplay = `${this.previous}${number}`;
               console.log("this.calculatorDisplay", this.calculatorDisplay);
@@ -68,8 +70,12 @@
             } else if (this.operatorClicked === "-") {
               this.calculatorDisplay = `${this.previous}${number}`;
               console.log("this.calculatorDisplay", this.calculatorDisplay);
-              this.interimResult = Number(this.interimResult) - Number(number);
-              }
+              this.interimResult = Number(this.interimResult) - Number(number);            
+            } else if (this.operatorClicked === "*") {
+              this.calculatorDisplay = `${this.previous}${number}`;
+              console.log("this.calculatorDisplay", this.calculatorDisplay);
+              this.interimResult = Number(this.interimResult) * Number(number);
+            }
           this.setPrevious();
 
           } else if(!this.operatorWasClicked && this.previous !== null){
@@ -93,18 +99,34 @@
             this.calculatorDisplay = `${this.calculatorDisplay}.`;
           }
           this.setPrevious();
-          console.log("this.calculatorDisplay", this.calculatorDisplay);
         },   
 
-        add(operator){          
-          this.interimResult = this.calculatorDisplay;
-          this.calculatorDisplay = `${this.calculatorDisplay}${operator}`;
-          this.operatorClicked = operator;
-          this.setPrevious();
-          this.operatorWasClicked = true;
+        operate(operator){     
+          if (!this.operatorWasClicked && this.calculatorDisplay === ""){
+            console.log("first case");
+            this.interimResult = 0;
+            this.calculatorDisplay = `0${operator}`;
+            this.operatorClicked = operator;
+            this.setPrevious();
+            this.operatorWasClicked = true;
+            this.isAwaitingNumber = true;
+          } else if(!this.operatorWasClicked){
+            console.log("second case");
+            this.interimResult = this.calculatorDisplay;
+            this.calculatorDisplay = `${this.calculatorDisplay}${operator}`;
+            this.operatorClicked = operator;
+            this.setPrevious();
+            this.operatorWasClicked = true;
+            this.isAwaitingNumber = true;
+          } else {
+            console.log("third case");
+            return;
+          }  
+          console.log("interimResult", this.interimResult);
+          console.log("calculatorDisplay", this.calculatorDisplay);        
         },
         
-        calculate(){
+        displayResult(){
           this.calculatorDisplay = this.interimResult;
         },
     }
